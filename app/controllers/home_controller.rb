ecedc4e3038858647d01c729cfacb11f0e7ab95a
りんご <aochaaaaannnn@aokishuacBookea.lan>
2018-06-10 14:39:31 +0900

    first commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,14 @@
+class HomeController < ApplicationController
+  def top
+  end
+
+  def ranking_month
+  end
+
+  def ranking_week
+  end
+
+  def ranking_year
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root 'home#top'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'ranking_week' => 'home#ranking_week'
+  get 'ranking_month' => 'home#ranking_month'
+  get 'ranking_year' => 'home#ranking_year'
+end
